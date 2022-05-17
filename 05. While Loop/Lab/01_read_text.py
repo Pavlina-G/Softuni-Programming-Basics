49000bb1004ad33875709bad304283039136a0d4
Pavlina-G <102150683+Pavlina-G@users.noreply.github.com>
2022-05-17 13:22:32 +0300

    Create 01_read_text.py

---
+++ 05. While Loop/Lab/01_read_text.py
@@ -0,0 +1,12 @@
+while True:
+    text = input()
+    if text == "Stop":
+        break
+    print(text)
+
+# text = ""
+# while text != "Stop":
+#     text = input()
+#     if text == "Stop":
+#         break
+#     print(text)
